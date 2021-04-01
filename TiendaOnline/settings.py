41db4448ad309c2bbb9b2b753ec4ef8e46fc495a
anthonyquiranza <anthony.quiranza@upec.edu.ec>
2021-04-01 16:00:04 -0500

    Change on settings: sqlite to postgreSQL and configuration of AdminPanel

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'articulosclientes',
+        'USER':'Anthony@anthonyquiranza',
+        'PASSWORD':'postgresql123..',
+        'HOST':'anthonyquiranza.postgres.database.azure.com',
+        'DATABASE_PORT':'5432'
     }
 }
 
