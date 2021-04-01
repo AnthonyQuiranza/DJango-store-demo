03355b86ac04b3d198e8265f80976766d8263d1b
anthonyquiranza <anthony.quiranza@upec.edu.ec>
2021-04-01 18:02:16 -0500

    django admin panel changed to spanish

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from gestionPedidos.models import Clientes, Articulos, Pedidos
 # Register your models here.
 
-admin.site.register(Clientes)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+class clientesAdmin(admin.ModelAdmin):
+    list_display=("nombre","email","tfno")
+    search_fields=("nombre","tfno")
+
+class articulosAdmin(admin.ModelAdmin):
+    list_filter=("seccion",)
+
+class pedidosAdmin(admin.ModelAdmin):
+    list_display=("numero","fecha")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+
+admin.site.register(Clientes,clientesAdmin)
+admin.site.register(Articulos,articulosAdmin)
+admin.site.register(Pedidos,pedidosAdmin)
 
